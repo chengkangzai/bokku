3eb52f4d52244611b4d120bb0e182ab19d67acd5
Ching Cheng Kang <pycck@hotmail.com>
2026-03-31 12:57:21 +0800

    improve ux

--- app/Filament/Resources/Accounts/AccountResource.php
+++ app/Filament/Resources/Accounts/AccountResource.php
@@ -77,6 +77,7 @@ class AccountResource extends Resource
                                         default => 'Starting balance for this account'
                                     }
                             )
+                            ->formatStateUsing(fn (?string $state) => $state ? number_format($state, 2) : null)
                             ->disabled(fn (Component $livewire) => $livewire instanceof EditAccount)
                             ->afterContent(
                                 Action::make('adjustBalance')
@@ -151,7 +152,7 @@ class AccountResource extends Resource
                             ->prefix(fn (?Account $record) => $record?->currency)
                             ->disabled()
                             ->dehydrated(false)
-                            ->formatStateUsing(fn (?Account $record) => $record ? number_format($record->balance, 2) : null)
+                            ->formatStateUsing(fn (?string $state) => $state ? number_format($state, 2) : null)
                             ->visible(fn (Component $livewire) => $livewire instanceof EditAccount),
 
                         Select::make('currency')
